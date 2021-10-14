26f495b27add8fd0735b8fcc75fe26455df1ea91
tphilippe2021 <thomasphilippe1988@gmail.com>
2021-10-14 16:02:01 +0200

    Version6

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'accueil')]
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
+        $sorties = $sortieRepository->findAll();
+
+
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            "sorties" => $sorties
         ]);
     }
 }
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -7,8 +7,10 @@ use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Entity\Ville;
 
+
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,7 +24,7 @@ class SortieType extends AbstractType
 
                 "label"=> "Nom de la sortie :"
             ])
-            ->add('dateHeureDebut',DateType::class, [
+            ->add('dateHeureDebut',DateTimeType::class, [
                 'widget' => 'choice',
                 "label"=> "Date et heure de la sortie :"
             ])
